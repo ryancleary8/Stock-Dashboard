7b059bd9a1958c2319bfa318709f647014258e43
ryancleary8 <rycy816@gmail.com>
2026-04-13 16:47:27 -0700

    almost

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-router-dom': fileURLToPath(new URL('./src/shims/react-router-dom.jsx', import.meta.url))
-    }
-  },
   server: {
     port: 5173,
     proxy: {
